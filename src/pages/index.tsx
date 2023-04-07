03835c2b1296af19444c2f45c047982e60bfa08d
Wiwek09 <bibekwiwek54@gmail.com>
2023-04-07 07:25:23 +0545

    changes made

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,18 +46,18 @@ export default function Home() {
        <h2 className='md:text-xl text-gray-300 cursor-pointer  hover:text-lime-700 ml-6 pt-3' >Home</h2>
        <h2 className='md:text-xl text-gray-300  cursor-pointer hover:text-lime-700 ml-6 pt-3' >Join</h2>
       </nav>
-      <div className='md:pt-32 md:pl-32 md:h-auto pt-10 '>
-        <section className='md:w-[50%] w-full ' >
+      <div className='md:pt-32 md:pl-32 md:h-auto pt-10 md:w-[100%] '>
+        <section className='md:w-[50%]' >
           <h2 className='uppercase text-sm text-gray-400 md:text-xl -mb-6' >Start for free</h2>
           <h1 className='text-xl md:text-3xl text-white' >Create new account<span className='text-blue-800 text-6xl font-bold ' >.</span></h1>
           <h2 className='mt-3 text-gray-300' >Already A  Member?<span className='text-red-500 cursor-pointer font-bold md:text-2xl  hover:text-red-400 text-xl ' > Log In</span></h2>
         </section>
-        <section className='mt-7 h-full'>
-          <form onSubmit={onSubmit} className='w-[50%]' method="post"  >
-            <div className='md:flex md:gap-6 w-full ' >
-               <div className='relative h-[6vh]' >  
+        <section className='mt-7 h-full w-[60%] '>
+          <form onSubmit={onSubmit} className='' method="post"  >
+            <div className='md:flex flex-col gap-6 md:gap-6 w-full  ' >
+               <div className='relative h-[6vh] w-full ' >  
                  <label className='absolute top-0 px-2 translate-x-1 text-xs text-gray-800 focus:text-blue-800' >First Name</label>
-                 <input {...register("firstName",{required:true,pattern: /^[A-Za-z]+$/, maxLength: 20})} id='firstName' className='outline-none px-2 py-[1.5rem] border-2 rounded-md text-justify h-[5vh]  focus:border-blue-400 bg-gray-300 pr-10' type={"text"} />
+                 <input {...register("firstName",{required:true,pattern: /^[A-Za-z]+$/, maxLength: 20})} id='firstName' className='outline-none px-2 py-[1.5rem] border-2 rounded-md text-justify h-[5vh]  focus:border-blue-400 bg-gray-300 pr-10 w-full' type={"text"} />
                  {
                   errors?.firstName?.type === "required" && <p className="text-red-600" >This field is required</p>
                  }   
@@ -69,9 +69,9 @@ export default function Home() {
                  }                              
                 <GrContactInfo className='text-xl absolute top-1/2 right-2 -translate-x-1 -translate-y-1/2'/>
                </div>
-               <div className='relative h-[6vh]' >
+               <div className='relative h-[6vh] w-full ' >
                  <label className='absolute top-0 translate-x-1 px-2  text-xs text-gray-800 focus:text-blue-800 ' >Last Name</label>
-                 <input {...register("lastName",{required:true,pattern: /^[A-Za-z]+$/})} id='LastName' className='outline-none border-2 px-2 py-[1.5rem] rounded-md h-[5vh] bg-gray-300 focus:border-blue-400 pr-10' type={"text"}/>
+                 <input {...register("lastName",{required:true,pattern: /^[A-Za-z]+$/})} id='LastName' className='outline-none border-2 px-2 py-[1.5rem] rounded-md h-[5vh] bg-gray-300 focus:border-blue-400 pr-10 w-full' type={"text"}/>
                  {
                   errors?.lastName?.type === "required" && <p className="text-red-600" >This field is required</p>
                  }
@@ -81,7 +81,7 @@ export default function Home() {
                  <GrContactInfo className='text-xl absolute top-1/2 right-2 -translate-x-1 -translate-y-1/2 '/>
                </div>
             </div> 
-            <div className='mt-10  flex flex-col gap-3' >
+            <div className='mt-10  flex flex-col gap-5' >
                <div className='relative h-[6vh]' >
                  <label className='absolute top-0 px-2  translate-x-1 text-xs text-gray-800 focus:text-blue-800 ' >Email</label>
                  <input {...register("email",{required:true})} id='email' className='outline-none h-[5vh] px-2 py-[1.5rem] w-full border-2 rounded-md  bg-gray-300 focus:border-blue-400 pr-9' type={"email"}/>
